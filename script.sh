a6b59b765d6f2e12a556c6a10813e89cde4c8abb
rodrilucas <lucas.celery@gmail.com>
2025-03-31 20:06:16 -0300

    [feat]: install vscode and extensions

---
+++ script.sh
@@ -0,0 +1,17 @@
+sudo dnf update && dnf upgrade
+
+if sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc; then
+    echo "Importando key..."
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
+else
+    echo "Falha ao importar a key. Verifique a conexão ou o link!"
+fi
+
+echo "Instalando extensões"
+
+while IFS= read -r line; do
+    extension=${line#*=}
+    code --install-extension ${extension%,*}
+done <extensions
+
+echo "Extensões instaladas com sucesso."
