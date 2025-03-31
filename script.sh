0a8758cc7aa36afc59ee9eb3091c225a938c4c95
rodrilucas <lucas.celery@gmail.com>
2025-03-31 20:59:38 -0300

    [feat]: install idea

--- script.sh
+++ script.sh
@@ -20,7 +20,7 @@ echo "Extensões do vscode instaladas com sucesso."
 
 echo "Instalando google chrome"
 
-version=${uname -m}
+version=${uname-m}
 
 if wget https://dl.google.com/linux/direct/google-chrome-stable_current_${version}.rpm; then
     echo "Baixando e instalando o google chrome"
@@ -31,3 +31,21 @@ else
 fi
 
 echo "Google chrome instalado com sucesso"
+
+echo "Instalando Intellij idea"
+
+if wget -O ./ideaIC-2024.3.5.tar.gz https://download.jetbrains.com/idea/ideaIC-2024.3.5.tar.gz?_gl=1*1wmjk5v*_ga*NTYyMDgxODQ3LjE3NDM0NjMzMDI.*_ga_9J976DJZ68*MTc0MzQ2MzMwMS4xLjEuMTc0MzQ2NDA0Mi4wLjAuMA..; then
+    tar -xvf ./ideaIC-2024.3.5.tar.gz
+else
+    echo "Falha ao baixar intellij idea community"
+fi
+
+shortcut="IntellijIdea.desktop"
+
+touch $shortcut
+
+while IFS= read -r line; do
+    printf "%s\n" "$line" >> ./$shortcut
+done < idea
+
+mv ./IntellijIdea.desktop ~/.local/share/applications
