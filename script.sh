7a3a53b072a89ad7b611c10a28b01616199da136
rodrilucas <lucas.celery@gmail.com>
2025-04-01 22:41:58 -0300

    [refactor]: echo

--- script.sh
+++ script.sh
@@ -2,8 +2,6 @@
 
 sudo dnf update -y && sudo dnf upgrade -y
 
-echo "Instalando vscode"
-
 if sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc; then
     echo "Importando key..."
     echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
@@ -14,18 +12,10 @@ fi
 
 sudo dnf install -y code
 
-echo "Vscode instalado com sucesso"
-
-echo "Instalando extensões do vscode"
-
 while IFS= read -r line; do
     extension=${line#*=}
     code --install-extension "${extension%,*}"
 done < extensions
-
-echo "Extensões do vscode instaladas com sucesso."
-
-echo "Instalando google chrome"
 
 version=$(uname -m)
 
@@ -37,10 +27,6 @@ else
     echo "Falha ao baixar o google chrome. Verifique a conexão ou o link"
     exit 1
 fi
-
-echo "Google chrome instalado com sucesso"
-
-echo "Instalando Intellij idea"
 
 if wget -q -O ./ideaIC-2024.3.5.tar.gz https://download.jetbrains.com/idea/ideaIC-2024.3.5.tar.gz?_gl=1*1wmjk5v*_ga*NTYyMDgxODQ3LjE3NDM0NjMzMDI.*_ga_9J976DJZ68*MTc0MzQ2MzMwMS4xLjEuMTc0MzQ2NDA0Mi4wLjAuMA..; then
     tar -xvf ./ideaIC-2024.3.5.tar.gz
@@ -65,15 +51,9 @@ fi
 
 mv ./"$shortcut" "${local}"
 
-echo "Intellij Idea instalado com sucesso"
-
-echo "Instalando warp"
-
 if ! command -v warp &>/dev/null; then
     echo "warp não está instalado. Instalando..."
     sudo dnf install -y warp
 else
     echo "warp já está instalado"
 fi
-
-echo "Warp instalado com sucesso"
