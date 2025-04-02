265e7aae276d1d0530ba101883c5d1b223c0bb80
rodrilucas <lucas.celery@gmail.com>
2025-04-02 19:57:58 -0300

    [refactor]: remove ifs

--- script.sh
+++ script.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+set -e
+
 sudo dnf update -y && sudo dnf upgrade -y
 
-if sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc; then
-    echo "Importando key..."
-    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
-else
-    echo "Falha ao importar a key. Verifique a conexão ou o link!"
-    exit 1
-fi
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc || exit 1
+echo "Importando key..."
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 
 sudo dnf install -y code
 
@@ -19,21 +17,12 @@ done <extensions
 
 version=$(uname -m)
 
-if wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_${version}.rpm; then
-    echo "Baixando e instalando o google chrome"
-    sudo dnf install -y ./google-chrome-stable_current_*.rpm
-    google-chrome --version
-else
-    echo "Falha ao baixar o google chrome. Verifique a conexão ou o link"
-    exit 1
-fi
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_${version}.rpm || exit 1
+sudo dnf install -y ./google-chrome-stable_current_*.rpm
+google-chrome --version
 
-if wget -q -O ./ideaIC-2024.3.5.tar.gz https://download.jetbrains.com/idea/ideaIC-2024.3.5.tar.gz?_gl=1*1wmjk5v*_ga*NTYyMDgxODQ3LjE3NDM0NjMzMDI.*_ga_9J976DJZ68*MTc0MzQ2MzMwMS4xLjEuMTc0MzQ2NDA0Mi4wLjAuMA..; then
-    tar -xvf ./ideaIC-2024.3.5.tar.gz
-else
-    echo "Falha ao baixar IntelliJ IDEA Community"
-    exit 1
-fi
+wget -O ./ideaIC-2024.3.5.tar.gz "https://download.jetbrains.com/idea/ideaIC-2024.3.5.tar.gz?_gl=1*1wmjk5v*_ga*NTYyMDgxODQ3LjE3NDM0NjMzMDI.*_ga_9J976DJZ68*MTc0MzQ2MzMwMS4xLjEuMTc0MzQ2NDA0Mi4wLjAuMA.." || exit 1
+tar -xvf ./ideaIC-2024.3.5.tar.gz
 
 shortcut="IntellijIdea.desktop"
 local="$HOME/.local/share/applications"
@@ -44,52 +33,26 @@ while IFS= read -r line; do
     printf "%s\n" "${line//\$HOME/$HOME}" >>"$shortcut"
 done <idea
 
-if [ ! -d "${local}" ]; then
-    echo "Diretório não existe, criando diretório..."
-    mkdir -p "${local}"
-fi
+mkdir -p "$local"
+mv ./$shortcut "$local"
 
-mv ./"$shortcut" "${local}"
-
-if ! command -v warp &>/dev/null; then
-    echo "warp não está instalado. Instalando..."
-    sudo dnf install -y warp
-else
-    echo "warp já está instalado"
-fi
+command -v warp &>/dev/null || sudo dnf install -y warp
 
 APPS=(
     "rest.insomnia.Insomnia"
 )
 
-if ! command -v flatpak &>/dev/null; then
-    echo "Flatpak não encontrado. Instalando..."
-    sudo dnf install -y flatpak
-fi
-
-if ! flatpak remote-list | grep -q "flathub"; then
-    echo "Adicionando repositório Flathub..."
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-fi
+command -v flatpak &>/dev/null || sudo dnf install -y flatpak
+flatpak remote-list | grep -q "flathub" || flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 for APP in "${APPS[@]}"; do
-    echo "Instalando $APP..."
     flatpak install -y flathub "$APP"
 done
 
 if ! command -v docker &>/dev/null; then
-    echo "Docker não encontrado. Instalando..."
     sudo dnf -y install dnf-plugins-core
-
-    if sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo; then
-        echo "Adicionando repositório docker..."
-        echo "Repositório adicionado com sucesso!"
-        sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    else
-        echo "Falha ao adicionar repositório. Verifique o link"
-        exit 1
-    fi
-
+    sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo || exit 1
+    sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 sudo systemctl enable --now docker
