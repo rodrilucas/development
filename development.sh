01f0e1be722bc9166e3d00daee743cea3eaf1bdf
rodrilucas <lucas.celery@gmail.com>
2025-04-09 09:17:33 -0300

    feat: install vscode

--- development.sh
+++ development.sh
@@ -8,3 +8,4 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc || exit 1
 echo "Importando key..."
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 
+sudo dnf install -y code
