84b7d5021fca83d168d138e2cafd51248f7ca6da
rodrilucas <lucas.celery@gmail.com>
2025-04-09 19:08:39 -0300

    feat: install code extensions

--- development.sh
+++ development.sh
@@ -9,3 +9,8 @@ echo "Importando key..."
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 
 sudo dnf install -y code
+
+while IFS= read -r line; do
+    extension=${line#*=}
+    code --install-extension "${extension%,*}"
+done <extensions
